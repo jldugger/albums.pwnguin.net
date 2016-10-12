bd071cf29e71ee69a42715f4f072066b270e7748
Justin Dugger <jldugger@osuosl.org>
2016-10-11 23:19:09 -0700

    add sitemap.xml

--- album_project/urls.py
+++ album_project/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from photologue.sitemaps import GallerySitemap, PhotoSitemap
+
+sitemaps = {
+            'photologue_galleries': GallerySitemap,
+            'photologue_photos': PhotoSitemap,
+}
 
 urlpatterns = [
+    url(r'^sitemap\.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^photologue/', include('photologue.urls', namespace='photologue')),
     url(r'^$', TemplateView.as_view(template_name="homepage.html"), name='homepage'),
