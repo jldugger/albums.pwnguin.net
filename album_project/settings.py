f2aea3bb148a006a319f11af904bcae19ed1b077
Justin Dugger <jldugger@osuosl.org>
2017-01-14 20:47:49 -0800

    fix static content urls

--- album_project/settings.py
+++ album_project/settings.py
@@ -86,7 +86,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
-STATIC_URL = '/static/'
+STATIC_URL = '/albums/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public', 'media')
 MEDIA_URL = '/media/'
